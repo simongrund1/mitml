a6d288c0118e3a7b85d1aa60251652250d2ccc1f
Simon Grund <grund@ipn.uni-kiel.de>
2017-01-30 16:02:37 +0100

    Fixed output for fixed scale parameters/sigma

--- R/internal-getVC.R
+++ R/internal-getVC.R
@@ -11,9 +11,9 @@
   vlist <- addp <- NULL
 
   # variance components
+  vlist <- list()
   vc <- lapply(model,lme4::VarCorr)
   clus <- names(vc[[1]])
-  vlist <- list()
   for(vv in clus){
     q <- dim(vc[[1]][[vv]])[1]
     v.cl <- vapply(vc, function(z) z[[vv]], FUN.VALUE=matrix(0,q,q))
@@ -21,18 +21,23 @@
     dimnames(v.cl)[1:2] <- lapply(dimnames(vc[[1]][[vv]]), function(z) sub("^[(]Intercept[)]$","Intercept",z))
     vlist[[paste("|",vv,sep="")]] <- v.cl
   }
-  rv <- sapply(vc, function(z) attr(z,"sc")^2)
-  dim(rv) <- c(1,1,m)
-  dimnames(rv) <- list("Residual","Residual",NULL)
-  vlist <- c(vlist, list(rv))
+  # residual variance (if model uses scale)
+  usesc <- attr(vc[[1]], "useSc")
+  if(usesc){
+    rv <- sapply(vc, function(z) attr(z,"sc")^2)
+    dim(rv) <- c(1,1,m)
+    dimnames(rv) <- list("Residual","Residual",NULL)
+    vlist <- c(vlist, list(rv))
+  }
 
   # additional parameters
-  for(vv in clus){
-    if("(Intercept)"%in%colnames(vc[[1]][[vv]])){
-      iv <- sapply(vc, function(z) z[[vv]]["(Intercept)","(Intercept)"])
+  # 1. ICC (only single clustering)
+  if(usesc & length(clus)==1){
+    if("(Intercept)"%in%colnames(vc[[1]][[clus]])){
+      iv <- sapply(vc, function(z) z[[clus]]["(Intercept)","(Intercept)"])
       icc <- iv / (iv + rv[1,1,])
       addp <- c(addp, mean(icc))
-      names(addp) <- paste("ICC|",vv,sep="")
+      names(addp) <- paste("ICC|",clus,sep="")
     }
   }
 
@@ -46,27 +51,57 @@
   m <- length(model)
   vlist <- addp <- NULL
 
-  # variance components (single clustering)
-  vc <- lapply(model,nlme::getVarCov)
-  clus <- attr(vc[[1]],"group.levels")
-  q <- dim(vc[[1]])[1]
-  v.cl <- vapply(vc, identity, FUN.VALUE=matrix(0,q,q))
-  if(is.null(dim(v.cl))) dim(v.cl) <- c(1,1,m)
-  dimnames(v.cl)[1:2] <- lapply(dimnames(vc[[1]]), function(z) sub("^[(]Intercept[)]$","Intercept",z))
+  # variance components (single clustering, limited for multiple clustering)
+  cls <- class(model[[1]])[1]
+  clus <- names(model[[1]]$coefficients$random)
   vlist <- list()
-  vlist[[paste("|",clus,sep="")]] <- v.cl
-  # residual variance
-  rv <- sapply(model, function(z) z$sigma^2)
-  dim(rv) <- c(1,1,m)
-  dimnames(rv) <- list("Residual","Residual",NULL)
-  vlist <- c(vlist, list(rv))
+  # single cluster variable
+  if(cls=="lme" & length(clus)==1){
+    vc <- lapply(model,nlme::getVarCov)
+    clus <- attr(vc[[1]],"group.levels")
+    q <- dim(vc[[1]])[1]
+    v.cl <- vapply(vc, identity, FUN.VALUE=matrix(0,q,q))
+    if(is.null(dim(v.cl))) dim(v.cl) <- c(1,1,m)
+    dimnames(v.cl)[1:2] <- lapply(dimnames(vc[[1]]), function(z) sub("^[(]Intercept[)]$","Intercept",z))
+    vlist[[paste("|",clus,sep="")]] <- v.cl
+  }else{
+    vc <- lapply(model,nlme::VarCorr)
+    # by variable
+    for(vv in clus){
+      q <- dim(model[[1]]$coefficients$random[[vv]])[2]
+      if(length(clus)==1){
+        rind <- 1:q
+      }else{
+        rind <- grep( paste0("^",vv," =$"), rownames(vc[[1]]))
+        rind <- (rind+1):(rind+q)
+      }
+      # ... by term
+      for(qq in rind){
+        v.cl <- sapply(vc, function(x) as.numeric(x[qq,1]))
+        if(is.null(dim(v.cl))) dim(v.cl) <- c(1,1,m)
+        dimnames(v.cl)[1:2] <- list(sub("^[(]Intercept[)]$", "Intercept", rownames(vc[[1]])[qq]))
+        vlist[[paste("|",vv,sep="")]] <- v.cl
+      }
+    }
+  }
+  # residual variance (if estimated)
+  fixsigma <- attr(model[[1]]$modelStruct,"fixedSigma")
+  if(!fixsigma){
+    rv <- sapply(model, function(z) z$sigma^2)
+    dim(rv) <- c(1,1,m)
+    dimnames(rv) <- list("Residual","Residual",NULL)
+    vlist <- c(vlist, list(rv))
+  }
 
-  # additional parameters (pre-processed)
-  if("(Intercept)"%in%colnames(vc[[1]])){
-    iv <- sapply(vc, function(z) z["(Intercept)","(Intercept)"])
-    icc <- iv / (iv + rv[1,1,])
-    addp <- c(addp, mean(icc))
-    names(addp) <- paste("ICC|",clus,sep="")
+  # additional parameters
+  # 1. ICC (only lme, single clustering)
+  if(!fixsigma & cls=="lme" & length(clus)==1){
+    if("(Intercept)"%in%colnames(vc[[1]])){
+      iv <- sapply(vc, function(z) z["(Intercept)","(Intercept)"])
+      icc <- iv / (iv + rv[1,1,])
+      addp <- c(addp, mean(icc))
+      names(addp) <- paste("ICC|",clus,sep="")
+    }
   }
 
   list(vlist=vlist,addp=addp)

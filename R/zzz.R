5dc800e133c2039992422d329b248f879cd76042
Simon Grund <grund@ipn.uni-kiel.de>
2017-02-09 11:21:58 +0100

    Updated NEWS

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 .onAttach <- function(libname, pkgname) {
-    packageStartupMessage("*** This is beta software. Please report any bugs!")
+    packageStartupMessage("*** This is beta software. Please report any bugs!\n*** See the NEWS file for recent changes.")
 }
 

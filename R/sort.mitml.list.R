922d1648b5763bd4c23a4bcc69df3591573c815f
Simon Grund <grund@ipn.uni-kiel.de>
2017-03-14 15:12:32 +0100

    New functions (c, cbind, rbind, sort, subset)

---
+++ R/sort.mitml.list.R
@@ -0,0 +1,16 @@
+sort.mitml.list <- function(x, decreasing=FALSE, by, ...){
+# sort list of multiply imputed data sets
+
+  expr <- substitute(by)
+  args0 <- list(decreasing=decreasing, ...)
+
+  res <- lapply(x, function(i){
+    args <- eval(expr,i,parent.frame())
+    if(!is.list(args)) args <- list(args)
+    ind <- do.call("order", c(args,args0))
+    i[ind,]
+  })
+
+  as.mitml.list(res)
+  
+}
---
+++ R/subset.mitml.list.R
@@ -0,0 +1,30 @@
+subset.mitml.list <- function(x, subset, select, ...){
+# subset list of multiply imputed data sets
+# NOTE: code adapted from subset.data.frame (by Peter Dalgaard and Brian Ripley)
+
+  rind <- if (missing(subset)) {
+    lapply(x, function(i) rep(TRUE, nrow(i)))
+  } else {
+    ss <- substitute(subset)
+    rind <- lapply(x, function(i) eval(ss, i, parent.frame()))
+    if (!is.logical(unlist(rind))) stop("'subset' must be logical")
+    lapply(rind, function(i) i & !is.na(i))
+  }
+
+  cind <- if (missing(select)) {
+    lapply(x, function(i) TRUE)
+  } else {
+    nl <- lapply(x, function(i){
+      l <- as.list(seq_along(i))
+      names(l) <- names(i)
+      l
+    })
+    se <- substitute(select)
+    lapply(nl, function(i) eval(se, i, parent.frame()))
+  }
+
+  res <- lapply(seq_along(x), function(i) x[[i]][rind[[i]], cind[[i]], drop=FALSE])
+  as.mitml.list(res)
+  
+}
+

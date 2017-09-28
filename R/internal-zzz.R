58ae24471532c73a82e655ef22b698ed544c1383
Simon Grund <grund@ipn.uni-kiel.de>
2017-09-28 15:50:42 +0200

    Re-organize internal functions

--- R/internal-zzz.R
+++ R/internal-zzz.R
@@ -1,6 +1,5 @@
 .extractMatrix <- function(x, ...){
-# x: an array of at least two dimensions
-# ...: indices beyond the first two that reduce x to a matrix
+# extract submatrix from array (indexed by ...)
 
   if(is.null(dim(x))) return(x)
 
@@ -11,3 +10,46 @@
   out
 
 }
+
+.adiag <- function(x, stacked=FALSE){
+# extract diagonal elements of first two dimensions in three-dimensional array
+# containing either square (default) or stacked matrices
+
+  d <- dim(x)
+
+  # indices for diagonal entries (square or stacked-square)
+  if(stacked){
+    i <- seq_len(d[2]) + d[1]*(seq_len(d[2])-1)
+    i <- outer(i,(seq_len(d[1]/d[2])-1)*d[2],`+`)
+    i <- outer(i,(seq_len(d[3])-1)*d[1]*d[2],`+`)
+  }else{
+    i <- seq_len(d[1]) + d[1]*(seq_len(d[1])-1)
+    i <- outer(i,(seq_len(d[3])-1)*d[1]^2,`+`)
+  }
+
+  x[as.vector(i)]
+
+}
+
+.check.modelL2 <- function(x){
+# check number of model equations
+
+  if(is.list(x) & length(x)>2)
+    stop("Cannot determine the number of levels. The 'formula' or 'type' argument must indicate either a single model for responses at level 1, or two models for responses at level 1 and 2.")
+
+  if(is.list(x) & length(x)==1) x <- x[[1]] # unlist
+  isL2 <- is.list(x) & length(x)==2
+
+  attr(x,"is.L2") <- isL2
+  x
+
+}
+
+.check.variablesL2 <- function(x,clus){
+# check for variables at L2 (constant at L1)
+
+  apply(x, 2, function(a) all( abs(a-clusterMeans(a,clus)) < sqrt(.Machine$double.eps),
+                               na.rm=T))
+
+}
+

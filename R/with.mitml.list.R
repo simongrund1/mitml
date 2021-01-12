ae4e6307113c2d66cb6918f605d4a8919f291d09
Simon Grund <grund@ipn.uni-kiel.de>
2021-01-12 13:14:03 +0100

    rename and revise include-data argument

--- R/with.mitml.list.R
+++ R/with.mitml.list.R
@@ -1,17 +1,21 @@
-with.mitml.list <- function(data, expr, use.data = FALSE, ...){
+with.mitml.list <- function(data, expr, include.data = FALSE, ...){
 # evaluates an expression for a list of data sets
 
   expr <- substitute(expr)
   pf <- parent.frame()
 
-  out <- lapply(data, function(d, expr, pf, use.data){
-    if(use.data){
+  out <- if(include.data){
+
+    lapply(data, function(d, expr){
       expr[["data"]] <- substitute(d)
       eval(expr, parent.frame())
-    }else{
-      eval(expr, d, pf)
-    }
-  }, expr = expr, pf = pf, use.data = use.data)
+    }, expr = expr)
+
+  }else{
+
+    lapply(data, function(d, expr, pf) eval(expr, d, pf), expr = expr, pf = pf)
+
+  }
 
   class(out) <- c("mitml.result", "list")
   return(out)
